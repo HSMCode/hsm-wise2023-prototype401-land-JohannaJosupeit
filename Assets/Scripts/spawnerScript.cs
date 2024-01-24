32b6b0a789b1c9c6295ed1f3d51fd17e5ee3fb81
Johanna <josupeitjohanna@gmail.com>
2024-01-24 23:05:59 +0100

    background and improved ui

--- Assets/Scripts/spawnerScript.cs
+++ Assets/Scripts/spawnerScript.cs
@@ -9,7 +9,9 @@ public class spawnerScript : MonoBehaviour
 {
     public GameObject spawnObject;
     public GameObject gameOverScreen;
+    public GameObject tutorial;
     public Camera mainCamera;
+    public GameObject background;
     public static int rounds;
     public static bool spawning;
     private Vector3 camPos;
@@ -34,13 +36,13 @@ public class spawnerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
         if (spawning && isAlive)
         {
             Instantiate(spawnObject, transform.position, Quaternion.identity);
             rounds++;
             distance++;
             spawning = false;
+            background.transform.localScale -= new Vector3(0.3f, 0.3f, 0.3f);
             mainCamera.orthographicSize += 1f;
             mainCamera.transform.position += new Vector3(0, 0.7f, 0);
             transform.position += new Vector3(0, 2f, 0);
@@ -57,6 +59,10 @@ public class spawnerScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && !isAlive)
         {
             EditorSceneManager.LoadScene("LandGame");
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            tutorial.SetActive(false);
         }
     }
 
